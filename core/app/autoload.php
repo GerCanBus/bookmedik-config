fd888dca2578abd092cd473717cf5193a488a350
victoria <113260020+victoria-nr@users.noreply.github.com>
2024-10-10 11:14:55 +0200

    Actualizar autoload.php cambiar función autoload por spl_autoload_register

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -1,14 +1,14 @@
 <?php
 // autoload.php
-// 10 octubre del 2014
+// 10 octubre del 2024
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
 
-function __autoload($modelname){
-	if(Model::exists($modelname)){
-		include Model::getFullPath($modelname);
-	} 
-}
+spl_autoload_register(function($modelname){
+        if(Model::exists($modelname)){
+                include Model::getFullPath($modelname);
+        } 
+});
 
 
 
