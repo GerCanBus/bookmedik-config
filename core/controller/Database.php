fd3a62c3f37697a66b7fad6c39f8f2f640c6f0b5
GerCanBus <gercanbus@alu.edu.gva.es>
2025-10-24 17:09:49 +0000

    Actualizando configuraciones

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -12,45 +12,7 @@ class Database {
         // ******** MODIFICO "USER" , "PASS" , DB   ***************
         // ********************************************************
 
-// PASO 2
-
-	//  *******  "HARCODEADAS"	$con = new mysqli("localhost", "gerard", "123456", "bookmedik");
-
-
-
-
-// Ruta al fichero de configuracion (subimos dos niveles desde este archivo)
-$configPath = __DIR__ . '/../../config.ini';
-
-// Comprobamos que existe y se puede leer
-if (!is_readable($configPath)) {
-    die('Falta el fichero de configuracion config.ini o no tiene permisos de lectura.');
-}
-
-// Leemos el fichero de configuración
-$cfg = parse_ini_file($configPath, true, INI_SCANNER_TYPED);
-$db  = $cfg['database'] ?? [];
-
-// Cargamos los parámetros con valores por defecto por si faltan
-$host    = $db['host']    ?? 'localhost';
-$dbname  = $db['name']    ?? 'bookmedik';
-$user    = $db['user']    ?? 'gerard';
-$pass    = $db['pass']    ?? '123456';
-$charset = $db['charset'] ?? 'utf8mb4';
-
-// Creamos la conexion
-$con = new mysqli($host, $user, $pass, $dbname);
-if ($con->connect_error) {
-    die('Error de conexion a la base de datos: ' . $con->connect_error);
-}
-
-// Establecemos el juego de caracteres
-if (!$con->set_charset($charset)) {
-    error_log('No se pudo establecer el charset ' . $charset . ': ' . $con->error);
-}
-
-// ********  FIN "HARCOREADAS"  ****************
-
+		$con = new mysqli("localhost", "gerard", "123456", "bookmedik");
 		$con->query("set sql_mode=''");
 		return $con;
 	}
